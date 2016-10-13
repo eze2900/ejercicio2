b40dd8cc2f6422da741994e1aed4035aec68d74e
Ezequiel Calcagno <ezequiel.calcagno@sovanta.com>
2016-10-13 12:48:16 -0300

    commit 3

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 /**
  * Created by admin on 10/13/16.
  */
+var button = function () {
+    $('.footer-btn').on('click', function () {
+        if ($(this).text() != 'Open') {
+            $(this).text('Open');
+        } else {
+            $(this).text('Closed');
+        }
+        if ($(this).height() != 100) {
+            $(this).height(100);
+        } else {
+            $(this).height(200);
+        }
+    })
+}
+$(document).ready(button);
